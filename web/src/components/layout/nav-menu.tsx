f1a3ba5f8c22f10a340a2f0d0334066fabc027a6
biggieliten <filipisaksson00@gmail.com>
2026-09-01 14:54:13 +0200

    Add links

--- web/src/components/layout/nav-menu.tsx
+++ web/src/components/layout/nav-menu.tsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router";
-import { NavigationMenu } from "../ui/navigation-menu";
 
 export default function NavMenu() {
   return (
-    <NavigationMenu>
+    <>
       <Link to="shop">Shop</Link>
-      <Link to="Admin">Admin</Link>
-    </NavigationMenu>
+      <Link to="admin">Admin</Link>
+    </>
   );
 }
