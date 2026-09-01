dce728cd9e15df9b19088e1bcb3b6d1716bb6f2c
Jonas Persson <jonas.persson111@gmail.com>
2026-09-01 18:22:19 +0200

    updated header

--- web/src/components/layout/header.tsx
+++ web/src/components/layout/header.tsx
@@ -1,9 +1,11 @@
+import ShoppingCartDrawer from "../shopping-cart-drawer";
 import NavMenu from "./nav-menu";
 
 export default function Header() {
   return (
-    <>
+    <header className="flex justify-between">
       <NavMenu />
-    </>
+      <ShoppingCartDrawer />
+    </header>
   );
 }
