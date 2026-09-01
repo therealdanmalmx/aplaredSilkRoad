c31fe88cac57dcb27e35acf11dcd92666ff51d65
Jonas Persson <jonas.persson111@gmail.com>
2026-09-01 18:24:25 +0200

    updated app.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import MainLayout from "./layouts/main-layout";
+import CheckoutPage from "./pages/checkout-page";
 function App() {
   return (
     // Flytta index-propertyn till indexsidans element när den är skapad och inlagd som child inuti routen för MainLayout.
     <BrowserRouter>
       <Routes>
-        <Route index element={<MainLayout />}></Route>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="checkout" element={<CheckoutPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
