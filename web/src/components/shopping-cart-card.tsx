30f42f890367927bfabdbd9c919c5b85e1a551c3
Jonas Persson <jonas.persson111@gmail.com>
2026-09-02 15:00:30 +0200

    use state för amount

--- web/src/components/shopping-cart-card.tsx
+++ web/src/components/shopping-cart-card.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import type { ShoppingCartItem } from "@/data/shopping-cart-item";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { useState } from "react";
 import { LuTrash2 } from "react-icons/lu";
 
 interface Props {
@@ -9,6 +11,7 @@ interface Props {
 
 export default function ShoppingCartCard(props: Props) {
   const isMobile = useIsMobile();
+  const [amount, setAmount] = useState(props.cartItem.amount);
 
   return (
     <article className="flex gap-1 mb-2 p-2 items-center justify-between">
@@ -21,7 +24,16 @@ export default function ShoppingCartCard(props: Props) {
         <div className="h-full text-left">
           <h3>{props.cartItem.name}</h3>
           <p>á {props.cartItem.price}</p>
-          <p>Amount: {props.cartItem.amount}</p>
+          <Input
+            type="number"
+            min={1}
+            max={99}
+            value={amount}
+            onChange={(e) => {
+              const value = Number(e.target.value);
+              setAmount(Math.min(99, Math.max(1, value)));
+            }}
+          />
           <p>Item Total: {props.cartItem.price * props.cartItem.amount}</p>
         </div>
       </div>
