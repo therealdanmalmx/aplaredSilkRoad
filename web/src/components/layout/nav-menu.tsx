27b508df05d1754f4ef5061dbaf91b8cb1302bd3
biggieliten <filipisaksson00@gmail.com>
2026-09-02 10:07:01 +0200

    re-initialized shadcn, fix missing css

--- web/src/components/layout/nav-menu.tsx
+++ web/src/components/layout/nav-menu.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router";
-
+import { Button } from "../ui/button";
 export default function NavMenu() {
   return (
     <>
-      <Link to="shop">Shop</Link>
-      <Link to="admin">Admin</Link>
+      <Button>
+        <Link to="shop">Shop</Link>
+      </Button>
+      <Button>
+        <Link to="admin">Admin</Link>
+      </Button>
     </>
   );
 }
