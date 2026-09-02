9bb54adb233aaab26c7e2d22ead2f504071ee725
therealdanmalmx <danmalmx@gmail.com>
2026-09-02 13:51:18 +0200

    Add product table

---
+++ web/src/components/product-table.tsx
@@ -0,0 +1,47 @@
+
+import { Button } from "@/components/ui/button";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import { LuPencil, LuTrash2 } from "react-icons/lu";
+import type { Product } from "../../../api/src/interfaces/admin";
+
+export function ProductTable({ products }: { products: Product[] }) {    
+  return (
+    <Table className="w-full md:w-10/12 mx-auto">
+      <TableHeader>
+        <TableRow>
+          <TableHead className="font-bold">Product</TableHead>
+          <TableHead className="font-bold">Name</TableHead>
+          <TableHead className="font-bold">Description</TableHead>
+          <TableHead className="font-bold">Slug</TableHead>
+          <TableHead className="font-bold">Price</TableHead>
+          <TableHead className="font-bold text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {products.map((product) => (
+            <TableRow key={product.id}>
+                <TableCell><img src={product.imageURL} alt={product.name} className="size-24 object-cover"/></TableCell>
+                <TableCell className="font-medium">{product.name}</TableCell>
+                <TableCell>{product.description}</TableCell>
+                <TableCell>{product.slug}</TableCell>
+                <TableCell>{product.price/100}kr</TableCell>
+                <TableCell className="text-right p-0">
+                    <Button className="cursor-pointer"><LuPencil /></Button>
+                    <Button className="cursor-pointer"><LuTrash2 /></Button>
+                </TableCell>
+            </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
+
+
+export default ProductTable;
