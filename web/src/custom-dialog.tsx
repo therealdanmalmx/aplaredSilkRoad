06dea8aeefb1c33007fcc27062a699093ee94704
therealdanmalmx <danmalmx@gmail.com>
2026-09-02 22:37:56 +0200

    Viewport button changes with css

--- web/src/custom-dialog.tsx
+++ web/src/custom-dialog.tsx
@@ -15,12 +15,8 @@ import { LuTrash2 } from "react-icons/lu";
 const CustomDialog = () => {
   return (
     <AlertDialog>
-        {window.innerWidth > 760 
-            ?
-                (<AlertDialogTrigger render={<Button variant="outline" className="cursor-pointer ml-1"><LuTrash2 /></Button>} />)
-            :
-                (<AlertDialogTrigger render={<Button variant="outline" className="rounded-none text-white w-full cursor-pointer bg-primary"><LuTrash2 />Delete</Button>} />)
-        }
+        <AlertDialogTrigger render={<Button variant="outline" className="cursor-pointer hidden md:inline-flex ml-1"><LuTrash2 /></Button>} />
+        <AlertDialogTrigger render={<Button variant="outline" className="rounded-none inline-flex md:hidden text-white w-full cursor-pointer bg-primary"><LuTrash2 />Delete</Button>} />
         <AlertDialogContent>
             <AlertDialogHeader>
             <AlertDialogTitle className="text-primary">Are you absolutely sure?</AlertDialogTitle>
@@ -30,7 +26,7 @@ const CustomDialog = () => {
             </AlertDialogHeader>
             <AlertDialogFooter>
             <AlertDialogCancel className="cursor-pointer">Cancel</AlertDialogCancel>
-            <AlertDialogAction variant="destructive" className="cursor-pointer text-background bg-destructive hover:text-destructive hover:bg-muted/50 hover:border-destructive">Delete</AlertDialogAction>
+            <AlertDialogAction variant="destructive" className="cursor-pointer text-background bg-destructive hover:text-destructive hover:bg-mu/50 hover:border-destructive">Delete</AlertDialogAction>
             </AlertDialogFooter>
         </AlertDialogContent>
         </AlertDialog>  )
