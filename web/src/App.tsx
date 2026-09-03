7bf5dfa3ff27079093551d14809c5bf6dffc9252
biggieliten <filipisaksson00@gmail.com>
2026-09-03 09:06:57 +0200

    Fix faulty routing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import MainLayout from "./layouts/main-layout";
+import AdminPage from "./pages/admin-page";
 import CheckoutPage from "./pages/checkout-page";
 import NewAdminProductPage from "./pages/new-admi-product-page";
 import StartPage from "./pages/start-page";
 
 function App() {
   return (
-    // Flytta index-propertyn till indexsidans element när den är skapad och inlagd som child inuti routen för MainLayout.
     <BrowserRouter>
       <Routes>
-        <Route element={<MainLayout />}>
+        <Route path="/" element={<MainLayout />}>
           <Route index element={<StartPage />} />
-          <Route path="/admin" element={<NewAdminProductPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="admin">
+            <Route index element={<AdminPage />} />
+            <Route path="add-product" element={<NewAdminProductPage />} />
+          </Route>
+          <Route path="checkout" element={<CheckoutPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
