6e6da1b2acc69401b887a1822fc906237701242f
therealdanmalmx <danmalmx@gmail.com>
2026-09-03 09:37:01 +0200

    Update filed import

--- web/src/pages/admin-page.tsx
+++ web/src/pages/admin-page.tsx
@@ -1,5 +1,5 @@
 
-import ProductCard from "@/components/product-card";
+import ProductCard from "@/components/admin-product-card";
 import ProductTable from "@/components/product-table";
 import { useEffect, useState } from "react";
 import { LuCirclePlus } from "react-icons/lu";
