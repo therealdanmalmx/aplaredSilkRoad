86e5e523a7b121733fe9d6f7191cc373ea0af806
therealdanmalmx <danmalmx@gmail.com>
2026-09-03 11:40:04 +0200

    Add link to update form

---
+++ web/src/components/admin-product-table.tsx
@@ -0,0 +1,51 @@
+
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import CustomDialog from "@/custom-dialog";
+import { LuPencil } from "react-icons/lu";
+import { Link } from "react-router";
+import type { Product } from "../../../api/src/interfaces/admin";
+
+export function AdminProductTable({ products }: { products: Product[] }) {    
+  return (
+    <Table className="w-full mx-auto">
+      <TableHeader>
+        <TableRow>
+          <TableHead className="text-xl text-primary font-bold">Product</TableHead>
+          <TableHead className="text-xl text-primary font-bold">Name</TableHead>
+          <TableHead className="text-xl text-primary font-bold">Description</TableHead>
+          <TableHead className="text-xl text-primary font-bold">Slug</TableHead>
+          <TableHead className="text-xl text-primary font-bold">Price</TableHead>
+          <TableHead className="text-xl text-primary font-bold text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {products.map((product) => (
+            <TableRow key={product.id}>
+                <TableCell><img src={product.imageURL} alt={product.name} className="size-24 object-cover"/></TableCell>
+                <TableCell className="font-medium">{product.name}</TableCell>
+                <TableCell>{product.description}</TableCell>
+                <TableCell>{product.slug}</TableCell>
+                <TableCell>{product.price/100}kr</TableCell>
+                <TableCell className="text-right p-0">
+                <Link to={`update-product/${product.id}`}>
+                  <Button className="cursor-pointer"><LuPencil /></Button>
+                </Link>
+                <CustomDialog />
+                </TableCell>
+            </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
+
+
+export default AdminProductTable;
