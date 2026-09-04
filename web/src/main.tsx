c2702f73130da6e970b8744c57e404e6daeb378b
therealdanmalmx <danmalmx@gmail.com>
2026-09-04 14:25:13 +0200

    Add 3sec duration for toaster

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,6 +7,8 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <Toaster />
+    <Toaster toastOptions={{
+      duration: 3000,
+    }}/>
   </StrictMode>,
 );
