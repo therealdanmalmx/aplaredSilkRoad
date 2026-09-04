d7ac54863e48edef030f3b7abf7df4aa1eae507d
biggieliten <filipisaksson00@gmail.com>
2026-09-04 13:32:32 +0200

    Install react query and implement fetch func in start page

--- web/src/pages/start-page.tsx
+++ web/src/pages/start-page.tsx
@@ -1,21 +1,23 @@
 import Hero from "@/components/hero";
 import ProductCard from "@/components/product-card";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { LuFishSymbol } from "react-icons/lu";
 import type { Product } from "../../../api/src/interfaces/admin";
 import heroImg from "../assets/silk-road-hero.png";
 
 export default function StartPage() {
   const [products, setProducts] = useState<Product[]>([]);
-  useEffect(() => {
-    const fetchProducts = async () => {
+
+  useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
       const res = await fetch("http://localhost:3000/admin");
       const data = await res.json();
 
       setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    },
+  });
 
   return (
     <>
