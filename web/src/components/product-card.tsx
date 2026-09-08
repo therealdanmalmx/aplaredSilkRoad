ceaeea30052a8ee1b589146762b6951b323b3a02
biggieliten <filipisaksson00@gmail.com>
2026-09-08 09:22:50 +0200

    Install jotai and implement cartAtom

--- web/src/components/product-card.tsx
+++ web/src/components/product-card.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardDescription,
@@ -5,24 +6,67 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useCartStorage } from "@/hooks/useCartStorage";
+import { useState } from "react";
+import { Link } from "react-router";
 import type { Product } from "../../../api/src/interfaces/admin";
 
 export default function ProductCard(product: Product) {
-  return (
-    <Card className="h-96 w-full pt-0">
-      <div className="aspect-video overflow-hidden">
-        <img
-          src={product.imageURL}
-          alt={product.name}
-          className="h-50 w-full object-cover"
-        />
-      </div>
+  const [amount, setAmount] = useState(1);
+  const { addItem } = useCartStorage();
 
-      <CardHeader>
-        <CardTitle>{product.name}</CardTitle>
-        <CardDescription>{product.description}</CardDescription>
-      </CardHeader>
-      <CardFooter></CardFooter>
+  function AddItemToCart() {
+    addItem({ id: product.id, amount });
+    setAmount(1);
+  }
+
+  return (
+    <Card className=" w-full pt-0">
+      <Link key={product.id} to={`/product/${product.id}`}>
+        <div className="aspect-video overflow-hidden">
+          <img
+            src={product.imageURL}
+            alt={product.name}
+            className="h-50 w-full object-cover"
+          />
+        </div>
+
+        <CardHeader className="p-2 min-h-44">
+          <CardTitle>{product.name}</CardTitle>
+          <CardDescription>
+            <b>{product.price} kr</b>
+            <p>{product.description}</p>
+          </CardDescription>
+        </CardHeader>
+      </Link>
+      <CardFooter className="grid items-center h-32">
+        <Button>
+          <Link to={`product/${product.id}`}>View</Link>
+        </Button>
+        <div className="flex justify-between w-full gap-4">
+          <Button
+            className="flex flex-1 max-w-24 sm:max-w-svh cursor-pointer"
+            onClick={() => AddItemToCart()}
+          >
+            Add to cart
+          </Button>
+          <Input
+            className="max-w-24"
+            type="number"
+            min={1}
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
+          />
+          {/* <input
+            className="max-w-10"
+            type="number"
+            min={1}
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
+          /> */}
+        </div>
+      </CardFooter>
     </Card>
   );
 }
--- web/src/pages/start-page.tsx
+++ web/src/pages/start-page.tsx
@@ -3,13 +3,16 @@ import Hero from "@/components/hero";
 import ProductCard from "@/components/product-card";
 import { useQuery } from "@tanstack/react-query";
 import { LuFishSymbol } from "react-icons/lu";
-import { Link } from "react-router";
 import { MoonLoader } from "react-spinners";
 import type { Product } from "../../../api/src/interfaces/admin";
 import heroImg from "../assets/silk-road-hero.png";
 
 export default function StartPage() {
-  const { data: products = [], isLoading, error } = useQuery({
+  const {
+    data: products = [],
+    isLoading,
+    error,
+  } = useQuery({
     queryKey: ["products"],
     queryFn: getProducts,
   });
@@ -35,10 +38,14 @@ export default function StartPage() {
             <div className="grid grid-cols-1 py-8 sm:grid-cols-2 sm:px-6 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5">
               {products.map((product: Product) => {
                 return (
-                  <Link key={product.id} to={`/product/${product.id}`}>
-                    <ProductCard {...product}></ProductCard>
-                  </Link>
-              );
+                  //   <Link
+                  //     className="z-10"
+                  //     key={product.id}
+                  //     to={`/product/${product.id}`}
+                  //   >
+                  <ProductCard {...product}></ProductCard>
+                  //   </Link>
+                );
               })}
             </div>
           )}
