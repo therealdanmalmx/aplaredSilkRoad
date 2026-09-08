da9b810440eb83b100db89adf0a9b8ec5582327b
biggieliten <filipisaksson00@gmail.com>
2026-09-08 09:51:10 +0200

    Remove checkout link from nav menu

--- web/src/pages/start-page.tsx
+++ web/src/pages/start-page.tsx
@@ -37,15 +37,7 @@ export default function StartPage() {
           {products && (
             <div className="grid grid-cols-1 py-8 sm:grid-cols-2 sm:px-6 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5">
               {products.map((product: Product) => {
-                return (
-                  //   <Link
-                  //     className="z-10"
-                  //     key={product.id}
-                  //     to={`/product/${product.id}`}
-                  //   >
-                  <ProductCard {...product}></ProductCard>
-                  //   </Link>
-                );
+                return <ProductCard {...product}></ProductCard>;
               })}
             </div>
           )}
