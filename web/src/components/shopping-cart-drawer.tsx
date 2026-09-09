c31734551ebe5ce499749d5ce5618d80b3ba94e6
Jonas Persson <jonas.persson111@gmail.com>
2026-09-09 13:26:18 +0200

    mobile cart now down

--- web/src/components/shopping-cart-drawer.tsx
+++ web/src/components/shopping-cart-drawer.tsx
@@ -28,7 +28,7 @@ export default function ShoppingCartDrawer() {
 
   return (
     <Drawer
-      swipeDirection="right"
+      swipeDirection={isMobile ? "down" : "right"}
       showSwipeHandle={isMobile}
       open={isOpen}
       onOpenChange={setIsOpen}
@@ -38,7 +38,7 @@ export default function ShoppingCartDrawer() {
           <LuShoppingCart className="text-primary text-2xl cursor-pointer" />
         }
       ></DrawerTrigger>
-      <DrawerContent className="h-full">
+      <DrawerContent className={"h-full " + (isMobile ? "" : "min-w-96")}>
         <DrawerHeader>
           <DrawerTitle className="text-primary">Shopping cart</DrawerTitle>
           <DrawerDescription>Manage your items</DrawerDescription>
