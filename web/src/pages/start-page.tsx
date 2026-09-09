f886cb6c3257c2248efc0d7da4c25992d7b29f14
therealdanmalmx <danmalmx@gmail.com>
2026-09-09 14:01:21 +0200

    Remove link on product card

--- web/src/pages/start-page.tsx
+++ web/src/pages/start-page.tsx
@@ -3,7 +3,6 @@ import Hero from "@/components/hero";
 import ProductCard from "@/components/product-card";
 import { useQuery } from "@tanstack/react-query";
 import { LuFishSymbol } from "react-icons/lu";
-import { Link } from "react-router";
 import { MoonLoader } from "react-spinners";
 import type { Product } from "../../../api/src/interfaces/admin";
 import heroImg from "../assets/silk-road-hero.png";
@@ -38,11 +37,7 @@ export default function StartPage() {
           {products && (
             <div className='grid grid-cols-1 py-8 sm:grid-cols-2 sm:px-6 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5'>
               {products.map((product: Product) => {
-                return (
-                  <Link key={product.id} to={`/product/${product.id}`}>
-                    <ProductCard {...product}></ProductCard>
-                  </Link>
-                );
+                return <ProductCard {...product}></ProductCard>;
               })}
             </div>
           )}
