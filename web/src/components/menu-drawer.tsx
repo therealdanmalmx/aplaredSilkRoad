1d8937dc93b84e1d0427d54a2ad6f5e35e381c3a
biggieliten <filipisaksson00@gmail.com>
2026-09-09 13:27:58 +0200

    Change swipe direction and placement

--- web/src/components/menu-drawer.tsx
+++ web/src/components/menu-drawer.tsx
@@ -18,17 +18,17 @@ export default function MenuDrawer() {
   ];
 
   return (
-    <Drawer swipeDirection="left" open={isOpen} onOpenChange={setIsOpen}>
+    <Drawer swipeDirection="down" open={isOpen} onOpenChange={setIsOpen}>
       <DrawerTrigger
         render={<LuMenu className="text-primary cursor-pointer text-2xl" />}
       />
-      <DrawerContent className="h-full bg-background border-none">
+      <DrawerContent className="h-auto bg-background border-none">
         <DrawerHeader className="bg-muted-foreground">
           <DrawerTitle className="font-eb-garamond font-bold text-2xl text-secondary-foreground mb-4">
             Explore Aplared Silk Road
           </DrawerTitle>
         </DrawerHeader>
-        <div className="flex flex-col mt-4">
+        <div className="flex flex-col py-4">
           {routes.map((r) => {
             return (
               <Link
