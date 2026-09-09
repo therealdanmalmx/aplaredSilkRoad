e481718fd3fe81a8d2241743d7b6e3936971e345
therealdanmalmx <danmalmx@gmail.com>
2026-09-09 13:40:44 +0200

    Add product promise

--- web/src/api/products.ts
+++ web/src/api/products.ts
@@ -1,4 +1,6 @@
-export async function getProducts() {
+import type { Product } from "../../../api/src/interfaces/admin";
+
+export async function getProducts(): Promise<Product[]> {
   const res = await fetch("http://localhost:3000/admin");
 
   const data = await res.json();
