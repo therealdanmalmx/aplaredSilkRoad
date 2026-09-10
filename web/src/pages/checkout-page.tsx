b9956241b548ba3a3343e14b8160ea68640d9bb2
Jonas Persson <jonas.persson111@gmail.com>
2026-09-10 17:52:53 +0200

    moved checkout form to own comp

--- web/src/pages/checkout-page.tsx
+++ web/src/pages/checkout-page.tsx
@@ -1,78 +1,14 @@
 import CheckoutCartCard from "@/components/checkout-cart-card";
+import CheckoutForm from "@/components/checkout-form";
 import { Button } from "@/components/ui/button";
-import {
-  Field,
-  FieldDescription,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-  FieldLegend,
-  FieldSeparator,
-  FieldSet,
-} from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { useCartStorage } from "@/hooks/useCartStorage";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Controller, useForm } from "react-hook-form";
 import { Link } from "react-router";
-import z from "zod";
-import {
-  customerSchema,
-  type Customer,
-} from "../../../shared/schemas/customerSchema";
-import {
-  createOrderSchema,
-  type CreateOrder,
-} from "../../../shared/schemas/orderSchema";
 
 export default function CheckoutPage() {
   const { cart } = useCartStorage();
 
-  const orderForm = useForm<z.infer<typeof customerSchema>>({
-    resolver: zodResolver(customerSchema),
-    shouldFocusError: false,
-    mode: "onSubmit",
-  });
-
-  const createOrder = async (customer: Customer) => {
-    // simulate fetch
-    await new Promise((resolve) => setTimeout(resolve, 1500));
-
-    const newOrder: CreateOrder = {
-      customer,
-      items: cart.map((ci) => ({ productId: ci.id, quantity: ci.amount })),
-    };
-    const validation = createOrderSchema.safeParse(newOrder);
-    if (!validation.success) {
-      console.log("failed validation", validation.error);
-      return;
-    }
-
-    console.log("Successful order", validation.data);
-    orderForm.reset();
-  };
-  const onInvalidOrder = (fieldErrors: any) => {
-    console.log("errors", fieldErrors);
-  };
-
   const isMobile = useIsMobile();
-  const countries = [
-    { label: "Sweden", value: "sweden" },
-    { label: "Denmark", value: "denmark" },
-    { label: "Finland", value: "finland" },
-    { label: "Iceland", value: "iceland" },
-    { label: "Norway", value: "norway" },
-  ];
 
   if (cart.length === 0) {
     return (
@@ -91,133 +27,7 @@ export default function CheckoutPage() {
         isMobile ? "m-4 flex flex-col" : "mx-auto m-4 flex w-fit gap-8"
       }
     >
-      <form
-        id="new-order-form"
-        className="flex flex-col gap-4"
-        onSubmit={orderForm.handleSubmit(createOrder, onInvalidOrder)}
-      >
-        <FieldSet>
-          <FieldLegend>Contact information</FieldLegend>
-          <FieldDescription>
-            We'll use this information to contact you about your order.
-          </FieldDescription>
-
-          <FieldGroup>
-            <div className="grid grid-cols-2 gap-4">
-              <Field>
-                <FieldLabel htmlFor="firstname">First name</FieldLabel>
-                <Input
-                  id="firstname"
-                  {...orderForm.register("firstName")}
-                  type="text"
-                  autoComplete="given-name"
-                />
-                <FieldError errors={[orderForm.formState.errors.firstName]} />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor="lastname">Last name</FieldLabel>
-                <Input
-                  id="lastname"
-                  {...orderForm.register("lastName")}
-                  type="text"
-                  autoComplete="family-name"
-                />
-                <FieldError errors={[orderForm.formState.errors.lastName]} />
-              </Field>
-            </div>
-            <Field>
-              <FieldLabel htmlFor="phone">Phone number</FieldLabel>
-              <Input
-                id="phone"
-                {...orderForm.register("phone")}
-                type="tel"
-                autoComplete="tel"
-              />
-              <FieldError errors={[orderForm.formState.errors.phone]} />
-            </Field>
-          </FieldGroup>
-        </FieldSet>
-        <FieldSeparator />
-        <FieldSet>
-          <FieldLegend>Delivery address</FieldLegend>
-          <FieldDescription>
-            We need information to deliver your order.
-          </FieldDescription>
-
-          <FieldGroup>
-            <Controller
-              control={orderForm.control}
-              name="address.country"
-              render={({ field, fieldState }) => (
-                <Field>
-                  <FieldLabel htmlFor="country">Country</FieldLabel>{" "}
-                  <Select
-                    value={field.value}
-                    onValueChange={field.onChange}
-                    items={countries}
-                  >
-                    <SelectTrigger className="w-full">
-                      <SelectValue placeholder="Select a country" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectLabel>Countries</SelectLabel>
-                        {countries.map((country) => (
-                          <SelectItem key={country.value} value={country.value}>
-                            {country.label}
-                          </SelectItem>
-                        ))}
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                  {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
-            <Field>
-              <FieldLabel htmlFor="city">City</FieldLabel>
-              <Input
-                id="city"
-                {...orderForm.register("address.city")}
-                type="text"
-                autoComplete="address-level2"
-              />
-              <FieldError errors={[orderForm.formState.errors.address?.city]} />
-            </Field>
-            <div className="grid grid-cols-2 gap-4">
-              <Field>
-                <FieldLabel htmlFor="street">Street</FieldLabel>
-                <Input
-                  id="street"
-                  {...orderForm.register("address.street")}
-                  type="text"
-                  autoComplete="street-address"
-                />
-                <FieldError
-                  errors={[orderForm.formState.errors.address?.street]}
-                />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor="zipcode">Zip code</FieldLabel>
-                <Input
-                  id="zipcode"
-                  {...orderForm.register("address.zipCode")}
-                  type="text"
-                  autoComplete="postal-code"
-                />
-                <FieldError
-                  errors={[orderForm.formState.errors.address?.zipCode]}
-                />
-              </Field>
-            </div>
-          </FieldGroup>
-        </FieldSet>
-        <Button type="submit" disabled={orderForm.formState.isSubmitting}>
-          {orderForm.formState.isSubmitting ? "Submitting..." : "Submit order"}
-        </Button>
-      </form>
+      <CheckoutForm />
       <div className={isMobile ? "-order-1" : ""}>
         <CheckoutCartCard />
       </div>
