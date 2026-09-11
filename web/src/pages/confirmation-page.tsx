ef9e290ff97bd2a88ee6650dd9ca11bf5de48715
biggieliten <filipisaksson00@gmail.com>
2026-09-11 13:17:07 +0200

    Init confirmation page

--- web/src/pages/confirmation-page.tsx
+++ web/src/pages/confirmation-page.tsx
@@ -1,7 +1,42 @@
+import getOnId from "@/api/generic";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router";
+import { MoonLoader } from "react-spinners";
+import type { Order } from "../../../shared/schemas/orderSchema";
+
 export default function ConfirmationPage() {
+  const { id } = useParams();
+  const ENDPOINT = "order";
+
+  const {
+    data: order,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["product"],
+    queryFn: () => getOnId<Order>(String(id), ENDPOINT),
+  });
+
   return (
-    <section>
-      <h1></h1>
-    </section>
+    <>
+      {isLoading && (
+        <MoonLoader className="mx-auto mt-24" color="#a87932" size={50} />
+      )}
+      {isError && <p>Could not find your treasures.</p>}
+      {order && (
+        <section>
+          <div>
+            <h1>
+              Your delicate selection from{" "}
+              <s className="opacity-70">the Great Silk Road</s> Aplared
+            </h1>
+            <p>
+              Your treasures await their journey and will soon reach the far
+              lands of {order.customer.address.city}
+            </p>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
