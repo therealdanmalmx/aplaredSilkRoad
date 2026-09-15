44c24f14beacec7cf71139fbf8bf05e68fc8b689
biggieliten <filipisaksson00@gmail.com>
2026-09-15 10:12:58 +0200

    Update fetch url

--- web/src/api/generic.ts
+++ web/src/api/generic.ts
@@ -2,7 +2,7 @@ export default async function getOnId<T>(
   id: string,
   endpoint: string,
 ): Promise<T> {
-  const res = await fetch(`http://localhost:3000/${endpoint}/${id}`);
+  const res = await fetch(`/api/v1/${endpoint}/${id}`);
 
   if (!res.ok) {
     throw new Error(`Fetch failed: ${res.status}`);
