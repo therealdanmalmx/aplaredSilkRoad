2c20c7b856e60c611cd72c47380603ed4ef4992a
biggieliten <filipisaksson00@gmail.com>
2026-09-17 11:13:24 +0200

    Replace error log conditional render

--- web/src/pages/start-page.tsx
+++ web/src/pages/start-page.tsx
@@ -12,31 +12,35 @@ export default function StartPage() {
     data: products = [],
     isLoading,
     isError,
-    error,
   } = useQuery({
     queryKey: ["products"],
     queryFn: getProducts,
   });
 
-  if (isError) {
-    console.error(`An error occursed: ${error.message}`);
-  }
   return (
     <>
       <Hero imgSource={heroImg} />
 
-      <section className='mx-auto grid w-full max-w-screen-2xl h-full px-4'>
+      <section className="mx-auto grid w-full max-w-screen-2xl h-full px-4">
         <>
-          <div className='flex flex-row justify-center items-center w-full max-w-screen-2xl pt-8 px-4 sm:px-6'>
-            <hr className='w-full text-primary' />
-            <LuFishSymbol className='mx-5 text-primary text-6xl' />
-            <hr className='w-full text-primary' />
+          <div className="flex flex-row justify-center items-center w-full max-w-screen-2xl pt-8 px-4 sm:px-6">
+            <hr className="w-full text-primary" />
+            <LuFishSymbol className="mx-5 text-primary text-6xl" />
+            <hr className="w-full text-primary" />
           </div>
+          {isError && (
+            <div className="flex flex-1 items-center justify-center font-eb-garamond px-4 py-16">
+              <p className="text-center font-eb-garamond text-4xl leading-tight text-primary sm:text-5xl">
+                We could find any treasures at this time. Please try again
+                later...
+              </p>
+            </div>
+          )}
           {isLoading && (
-            <MoonLoader className='mx-auto mt-24' color='#a87932' size={50} />
+            <MoonLoader className="mx-auto mt-24" color="#a87932" size={50} />
           )}
           {products && (
-            <div className='grid grid-cols-1 py-8 sm:grid-cols-2 sm:px-6 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5'>
+            <div className="grid grid-cols-1 py-8 sm:grid-cols-2 sm:px-6 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5">
               {products
                 .filter((product) => !product.isDeleted)
                 .map((product: Product) => {
